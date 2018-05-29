a1565a12dd32be628ae306328ce21633d5917acc
fishermen <chenbo@staff.weibo.com>
2018-05-29 14:54:11 +0800

    not limit post rate any more

--- libraries/chain/steem_evaluator.cpp
+++ libraries/chain/steem_evaluator.cpp
@@ -603,7 +603,8 @@ void comment_evaluator::do_apply( const comment_operation& o )
       if( _db.has_hardfork( STEEM_HARDFORK_0_12__176 ) )
       {
          if( o.parent_author == STEEM_ROOT_POST_PARENT )
-             FC_ASSERT( ( now - auth.last_root_post ) > STEEM_MIN_ROOT_COMMENT_INTERVAL, "You may only post once every 5 minutes.", ("now",now)("last_root_post", auth.last_root_post) );
+        	 	 // TODO： 不再检测频率了，随便发
+             // FC_ASSERT( ( now - auth.last_root_post ) > STEEM_MIN_ROOT_COMMENT_INTERVAL, "You may only post once every 5 minutes.", ("now",now)("last_root_post", auth.last_root_post) );
          else
              FC_ASSERT( (now - auth.last_post) > STEEM_MIN_REPLY_INTERVAL, "You may only comment once every 20 seconds.", ("now",now)("auth.last_post",auth.last_post) );
       }
